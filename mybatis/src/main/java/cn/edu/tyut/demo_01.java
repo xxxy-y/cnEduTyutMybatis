9ae3ecc91de9957004f27f964566002a4b04e091
羊羊 <wqt1922311386@outlook.com>
2023-10-26 12:33:05 +0800

    12:32 commit

--- mybatis/src/main/java/cn/edu/tyut/demo_01.java
+++ mybatis/src/main/java/cn/edu/tyut/demo_01.java
@@ -30,52 +30,58 @@ public class demo_01 {
             throw new RuntimeException(e);
         }
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession(true);
-        // 查找一个用户数据
-        int selectId = scanner.nextInt();
-        User selectUser = sqlSession.selectOne("User.getUserByIdOne", selectId);
-        System.out.println(selectUser);
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+            // 查找一个用户数据
+            int selectId = scanner.nextInt();
+            User selectUserById = sqlSession.selectOne("User.getUserByIdOne", selectId);
+            System.out.println(selectUserById);
 
-        // 插入一个用户数据
-        User insertUser = new User();
-        insertUser.setId(scanner.nextInt());
-        scanner.nextLine();
-        insertUser.setUsername(scanner.nextLine());
-        insertUser.setSex(scanner.nextLine());
-        insertUser.setAge(scanner.nextInt());
-        scanner.nextLine();
-        int result = sqlSession.insert("User.InsertUser", insertUser);
-        if (result > 0) {
-            System.out.println("User data was inserted successfully");
-        } else {
-            System.out.println("User data insertion failed");
+            // 模糊查询查找一个用户对象
+            String name = scanner.nextLine();
+            User selectUserByName = sqlSession.selectOne("User.getUserByNameOne", name);
+            System.out.println(selectUserByName);
+
+            // 插入一个用户数据
+            User insertUser = new User();
+            insertUser.setId(scanner.nextInt());
+            scanner.nextLine();
+            insertUser.setUsername(scanner.nextLine());
+            insertUser.setSex(scanner.nextLine());
+            insertUser.setAge(scanner.nextInt());
+            scanner.nextLine();
+            int result = sqlSession.insert("User.InsertUser", insertUser);
+            if (result > 0) {
+                System.out.println("User data was inserted successfully");
+            } else {
+                System.out.println("User data insertion failed");
+            }
+
+            // 修改一个对象数据
+            HashMap<String, Object> hashMap = new HashMap<>();
+            String hashUsername = scanner.nextLine();
+            hashMap.put("username", hashUsername);
+            Integer hashId = scanner.nextInt();
+            hashMap.put("id", hashId);
+            result = sqlSession.update("User.UpdateUser", hashMap);
+            if (result > 0) {
+                System.out.println("Updating the user data was successful");
+            } else {
+                System.out.println("Failed to update user data");
+            }
+
+            // 删除指定用户id的数据
+            int deleteId = scanner.nextInt();
+            result = sqlSession.delete("User.DeleteUser", deleteId);
+            if (result > 0) {
+                System.out.println("Deleting user data was successful");
+            } else {
+                System.out.println("Failed to delete user data");
+            }
+
+            // 查找全部用户数据
+            System.out.println("All user's details");
+            List<User> users = sqlSession.selectList("User.selectUsers");
+            users.forEach(System.out::println);
         }
-
-        // 修改一个对象数据
-        HashMap<String, Object> hashMap = new HashMap<>();
-        String hashUsername = scanner.nextLine();
-        hashMap.put("username", hashUsername);
-        Integer hashId = scanner.nextInt();
-        hashMap.put("id", hashId);
-        result = sqlSession.update("User.UpdateUser", hashMap);
-        if (result > 0) {
-            System.out.println("Updating the user data was successful");
-        } else {
-            System.out.println("Failed to update user data");
-        }
-
-        // 删除指定用户id的数据
-        int deleteId = scanner.nextInt();
-        result = sqlSession.delete("User.DeleteUser", deleteId);
-        if (result > 0) {
-            System.out.println("Deleting user data was successful");
-        } else {
-            System.out.println("Failed to delete user data");
-        }
-
-        // 查找全部用户数据
-        System.out.println("All user's details");
-        List<User> users = sqlSession.selectList("User.selectUsers");
-        users.forEach(System.out::println);
     }
 }
