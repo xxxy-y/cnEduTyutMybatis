b5bf5204aeaf39ce23193c6eb0053950f165e5d6
羊羊 <wqt1922311386@outlook.com>
2023-11-09 17:52:51 +0800

    17:52 commit

--- mybatis_trendsSQL/src/main/java/cn/edu/tyut/mapper/UserMapper.java
+++ mybatis_trendsSQL/src/main/java/cn/edu/tyut/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package cn.edu.tyut.mapper;
 
 import cn.edu.tyut.entity.User;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -13,5 +15,28 @@ import java.util.List;
  * @Package_Name cn.edu.tyut.mapper
  */
 public interface UserMapper {
-    List<User> selectUser();
+    List<User> ifSelectUser(User user);
+
+    List<User> whereSelectUser(User user);
+
+    List<User> chooseSelectUser(User user);
+
+    List<User> trimSelectUser(User user);
+
+    /**
+     * 使用set标签来更新用户数据
+     *
+     * @param user 更新后的用户属性
+     * @return 1 更新成功  0 更新失败
+     */
+    int setUpdateUser(User user);
+
+    List<User> findByArray(Integer[] array);
+
+    List<User> findByList(List<Integer> list);
+
+    /**
+     * 传递给MyBatis的Map对象需要在方法参数中明确声明为@Param注解
+      */
+    List<User> findByMap(@Param("map") HashMap<String, Object> hashMap);
 }
--- mybatis_trendsSQL/src/test/java/cn/edu/tyut/mapper/UserMapperTest.java
+++ mybatis_trendsSQL/src/test/java/cn/edu/tyut/mapper/UserMapperTest.java
@@ -5,6 +5,8 @@ import cn.edu.tyut.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -18,11 +20,93 @@ import java.util.List;
 class UserMapperTest {
 
     @Test
-    void selectUser() {
+    void ifSelectUser() {
         try (SqlSession sqlSession = MybatisUtils.getSqlsession(true)) {
             UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-            List<User> users = userMapper.selectUser();
+            User user = new User();
+            user.setSex("男");
+            List<User> users = userMapper.ifSelectUser(user);
             users.forEach(System.out::println);
+        }
+    }
+
+    @Test
+    void whereSelectUser() {
+        try (SqlSession sqlSession = MybatisUtils.getSqlsession(true)) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = new User();
+            user.setSex("男");
+            List<User> users = userMapper.whereSelectUser(user);
+            users.forEach(System.out::println);
+        }
+    }
+
+    @Test
+    void chooseSelectUser() {
+        try (SqlSession sqlSession = MybatisUtils.getSqlsession(true)) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = new User();
+            user.setSex("男");
+            List<User> users = userMapper.chooseSelectUser(user);
+            users.forEach(System.out::println);
+        }
+    }
+
+    @Test
+    void trimSelectUser() {
+        try (SqlSession sqlSession = MybatisUtils.getSqlsession(true)) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = new User();
+            user.setUsername("wqt");
+            user.setSex("男");
+            List<User> users = userMapper.trimSelectUser(user);
+            users.forEach(System.out::println);
+        }
+    }
+
+    @Test
+    void setUpdateUser() {
+        try (SqlSession sqlSession = MybatisUtils.getSqlsession(true)) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = new User();
+            user.setId(3);
+            user.setUsername("kk");
+            int i = userMapper.setUpdateUser(user);
+            System.out.println(i);
+        }
+    }
+
+    @Test
+    void findByArray() {
+        try (SqlSession sqlSession = MybatisUtils.getSqlsession(true)) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            Integer[] array = new Integer[]{1, 2, 3, 5, 14};
+            userMapper.findByArray(array).forEach(System.out::println);
+        }
+    }
+
+    @Test
+    void findByList() {
+        try (SqlSession sqlSession = MybatisUtils.getSqlsession(true)) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            List<Integer> list = new ArrayList<>();
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            list.add(14);
+            userMapper.findByList(list).forEach(System.out::println);
+        }
+    }
+
+    @Test
+    void findByMap() {
+        try (SqlSession sqlSession = MybatisUtils.getSqlsession(true)) {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            HashMap<String, Object> hashMap = new HashMap<>();
+            hashMap.put("id", 5);
+            hashMap.put("sex", "男");
+            List<User> user = userMapper.findByMap(hashMap);
+            user.forEach(System.out::println);
         }
     }
 }
