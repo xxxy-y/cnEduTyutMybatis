0bf649cc5bd2f586b1833e31b4be8f80c504b247
羊羊 <wqt1922311386@outlook.com>
2023-11-12 18:50:20 +0800

    18:50 commit

---
+++ mybatis_complexQuery_manyToMany/src/main/java/cn/edu/tyut/utils/MybatisUtils.java
@@ -0,0 +1,12 @@
+package cn.edu.tyut.utils;
+
+/**
+ * @Author wqt19
+ * @ClassName MybatisUtils
+ * @SubmitTime 周日
+ * @DATE 2023/11/12
+ * @Time 18:50
+ * @Package_Name cn.edu.tyut.utils
+ */
+public class MybatisUtils {
+}
